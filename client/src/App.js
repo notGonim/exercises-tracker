4c22dd4f08a066d32dfacd2432257c99606cc12f
notGonim <mhmodgonim96@gmail.com>
2021-03-06 15:47:53 +0200

    Added the navbar and config routes inside it

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { ExerciseList } from './components/ExerciseList';
+import { EditExercise } from './components/EditExercise';
+import { CreateExercise } from './components/CreateExercise';
+import { CreateUser } from './components/CreateUser';
+import { Navbar } from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Working</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar>
+          <Route path="/" exact component={ExerciseList} />
+          <Route path="/edit/:id" component={EditExercise} />
+          <Route path="/create" component={CreateExercise} />
+          <Route path="/user" component={CreateUser} />
+        </Navbar>
+      </div>
+    </Router>
   );
 }
 
